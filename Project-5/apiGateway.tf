5953e0b8d25c7ffbb201276b2702ab23fa1a3f44
jaswanthg76 <jaswanthg76@gmail.com>
2024-09-17 12:31:45 +0530

    tf

--- Project-5/apiGateway.tf
+++ Project-5/apiGateway.tf
@@ -4,34 +4,32 @@ resource "aws_apigatewayv2_api" "main" {
 }
 
 resource "aws_apigatewayv2_stage" "invoke" {
-    api_id = aws_apigatewayv2_api.main.id
-    name = "dev"
-    auto_deploy = true
-  
+  api_id     = aws_apigatewayv2_api.main.id
+  name       = "dev"
+  auto_deploy = true
 }
 
 resource "aws_apigatewayv2_integration" "main-integration" {
-  api_id           = aws_apigatewayv2_api.main.id
-  integration_type = "AWS_PROXY"
-
+  api_id             = aws_apigatewayv2_api.main.id
+  integration_type   = "AWS_PROXY"
   integration_method = "POST"
   integration_uri = aws_lambda_function.Api-invoke-lamdba.invoke_arn
 }
 
 resource "aws_apigatewayv2_route" "get" {
-  api_id = aws_apigatewayv2_api.main.id
-  route_key =  "GET /*"
-  target = "integrations/${aws_apigatewayv2_integration.main-integration.id}"
+  api_id    = aws_apigatewayv2_api.main.id
+  route_key = "GET /{proxy+}"
+  target    = "integrations/${aws_apigatewayv2_integration.main-integration.id}"
 }
 
-resource "aws_apigatewayv2_route" "get" {
-  api_id = aws_apigatewayv2_api.main.id
-  route_key =  "POST /*"
-  target = "integrations/${aws_apigatewayv2_integration.main-integration.id}"
+resource "aws_apigatewayv2_route" "post" {
+  api_id    = aws_apigatewayv2_api.main.id
+  route_key = "POST /{proxy+}"
+  target    = "integrations/${aws_apigatewayv2_integration.main-integration.id}"
 }
 
-resource "aws_apigatewayv2_route" "get" {
-  api_id = aws_apigatewayv2_api.main.id
-  route_key =  "PUT /*"
-  target = "integrations/${aws_apigatewayv2_integration.main-integration.id}"
+resource "aws_apigatewayv2_route" "put" {
+  api_id    = aws_apigatewayv2_api.main.id
+  route_key = "PUT /{proxy+}"
+  target    = "integrations/${aws_apigatewayv2_integration.main-integration.id}"
 }
